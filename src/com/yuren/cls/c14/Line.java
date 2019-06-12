475388095985ec3be3245d05b189106895326fae
dengdairiluo <1020447548@qq.com>
2019-06-12 23:16:28 +0800

    20190611ˆ program logic

---
+++ src/com/yuren/cls/c14/Line.java
@@ -0,0 +1,32 @@
+package com.yuren.cls.c14;
+
+import java.time.Year;
+
+/**
+ * Created with Intellij IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2019-06-12 23:11
+ */
+public class Line {
+    private Point start;
+    private Point end;
+
+    public Line(Point start, Point end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public double length() {
+        return start.distance(end);
+    }
+
+    public static void main(String[] args) {
+        Point start = new Point(2, 3);
+        Point end = new Point(3, 4);
+
+        Line line = new Line(start, end);
+        System.out.println(line.length());
+    }
+}
